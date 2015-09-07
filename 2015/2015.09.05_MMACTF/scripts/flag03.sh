b3ccbbfa0c3a6e008b568f04c05d8d2b106e5ddb
Ctf-Trinidad <ctf.trinidad@gmail.com>
2015-09-07 12:59:39 +0600

    changes

--- 2015/2015.09.05_MMACTF/scripts/flag03.sh
+++ 2015/2015.09.05_MMACTF/scripts/flag03.sh
@@ -1,39 +1,32 @@
 #!/bin/bash
 
-original=list03.txt
-reverse=list03.rev.txt
-result=result03.txt
-answer=theanswer03.txt
-curr=7D
-
+code=list.txt
+result=result.txt
+curr=4D
+rm -f $result
 function  xor()
 {
-  local res=(`echo "$1" | sed "s/../0x& /g"`)
-  shift 1
-  while [[ "$1" ]]; do
-      local one=(`echo "$1" | sed "s/../0x& /g"`)
-      local count1=${#res[@]}
-      if [ $count1 -lt ${#one[@]} ]
-      then
-            count1=${#one[@]}
-      fi
-      for (( i = 0; i < $count1; i++ ))
-      do
-            res[$i]=$((${one[$i]:-0} ^ ${res[$i]:-0}))
-      done
-      shift 1
-  done
-  printf "%02x" "${res[@]}"
+    local res=(`echo "$1" | sed "s/../0x& /g"`)
+    shift 1
+    while [[ "$1" ]]; do
+    local one=(`echo "$1" | sed "s/../0x& /g"`)
+    local count1=${#res[@]}
+    if [ $count1 -lt ${#one[@]} ]
+    then
+     count1=${#one[@]}
+        fi
+        for (( i = 0; i < $count1; i++ ))
+        do
+              res[$i]=$((${one[$i]:-0} ^ ${res[$i]:-0}))
+        done
+        shift 1
+    done
+    printf "%02x" "${res[@]}"
 }
 
-cat $original | sed '1!G;h;$!d'>$reverse
 while read i
 do
-echo `xor $i $curr`>>$result
-#echo $i $curr
-#xor $i $curr
-curr=`xor $i $curr`
-done < $reverse
-cat $result | sed '1!G;h;$!d' | tr '\n' ' '>$answer
-rm -f $result
-rm -f $reverse
+    echo `xor $i $curr` | tr '\n' ' '>>$result
+    curr=`xor $i $curr`
+done < $code
+
